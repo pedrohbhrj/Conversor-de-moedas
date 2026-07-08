59f6185f55dab1c9e0f66fb5abddebb7addf6d96
pedro <l2pedro157@gmail.com>
2026-07-08 17:14:03 -0300

    feat: adicionado design responsivo

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,5 +1,5 @@
 export default function Footer(){
     return <footer className="font-texto flex justify-center mt-32 pb-10 w-full h-120 rounded-[64px] bg-(--title-color)">
-        <span className="text-(--text-color) mt-46">© 2026 Conversor de moedas ,todos os direitos reservados.</span>
+        <span className="text-(--text-color) md:text-[24px] text-[12px] mt-30 md:mt-42">© 2026 Conversor de moedas ,todos os direitos reservados.</span>
     </footer>
 }
